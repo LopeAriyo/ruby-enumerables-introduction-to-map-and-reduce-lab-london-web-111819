16144198add92522e5609a25c40b6918411484a8
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-08 11:02:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,21 @@
 # My Code here....
+def map_to_negativize
+
+
+end
+
+def map_to_no_change
+
+end
+
+def map_to_double
+end
+
+
+def map_to_square
+
+  #result = [a**2, b**2, c**2, d**2]
+
+
+  #p result
+end
